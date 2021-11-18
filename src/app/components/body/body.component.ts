9d01a6d1636a6fb5602eb4cb41d7213f39b83b64
csolano87 <solanochristian6@gmail.com>
2021-11-18 16:53:01 -0500

    estructura footer y header

---
+++ src/app/components/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+
+
+@Component({
+
+    selector: 'app-body',
+    templateUrl: './body.component.html'
+
+})
+export class BodyComponent
+{
+
+
+
+}
---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,17 @@
+
+import { Template } from "@angular/compiler/src/render3/r3_ast";
+import { Component } from "@angular/core";
+
+
+@Component({
+
+    selector: 'app-footer',
+    templateUrl: './footer.component.html'
+
+})
+export class FooterComponent
+{
+
+
+
+}
